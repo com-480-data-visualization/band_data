b7b133970dc10d003db406d7bbc600fac4810302
charlottemeyr <charlotmeyer@gmail.com>
2025-04-15 13:40:33 +0200

    Landing page with title works. Additional cursor as tennis ball. Next steps will be to refractor the code to make it more modular

---
+++ website/js/main.js
@@ -0,0 +1,5 @@
+   // Wait for the DOM to be fully loaded
+   document.addEventListener('DOMContentLoaded', function() {
+    console.log('Page loaded and ready');
+    // Your JavaScript code will go here
+});
